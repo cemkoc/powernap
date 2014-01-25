83a6c0c04085205f8f39d65e23a2bb14c8babd46
Can <cankoc63@gmail.com>
2014-01-25 11:25:35 -0800

    changes

---
+++ accelerometer/android/maps_api/src/com/example/maps_api/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.maps_api;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void setContentView(int layoutResID) {
+    		
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
